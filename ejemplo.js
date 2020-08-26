a8615515a5a48e64e39ad3284b00856d97a5c1b9
luisaSilva06 <54030791+luisaSilva06@users.noreply.github.com>
2020-08-25 21:22:42 -0500

    Add files via upload

---
+++ ejemplo.js
@@ -0,0 +1,19 @@
+//---mensaje de alerta---
+//alert('hola')
+//para ingresar datos
+//
+
+function saludo(){
+    var n1;
+    n1=prompt('hola', 'Ingresa tu nombre');
+    alert("hola: "+n1)
+}
+
+function salida(){
+    alert('Que tenga buen dia')
+}
+
+//function saludo(){
+    //alert(´hola')
+    //alert('hola')
+//}
